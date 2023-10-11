09bbccde63a2955f021055d4a53153a9aeeb7ea3
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 17:47:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+//        int [] massif = {1,2,3,4,5};
+//        for (int i = massif.length; i>0 ; i--) {
+//            System.out.println(i);
+//
+//        }
+
+//        int [] array = {1,2,3,4,5};
+//        int sum = 0;
+//
+//        for (int i = 0; i < array.length ; i++) {
+//
+//
+//            sum += array[i];
+//
+//        }System.out.println(sum);
+
+
+                    //       int [] massif = new int[10];
+//        massif [0] = 10;
+//        massif [1] = 15;
+//        massif [2] = 20;
+//        massif [3] = 30;
+//        massif [4] = 40;
+//        massif [5] = 50;
+//        massif [6] = 60;
+//        massif [7] = 70;
+//        massif [8] = 80;
+//        massif [9] = 90;
+//        System.out.println(massif[0]);
+//        System.out.println(massif[1]);
+//        System.out.println(massif[2]);
+//        System.out.println(massif[3]);
+//        System.out.println(massif[4]);
+//        System.out.println(massif[5]);
+//        System.out.println(massif[6]);
+//        System.out.println(massif[7]);
+//        System.out.println(massif[8]);
+
+    }}
